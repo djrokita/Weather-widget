4273154d07ff69ba795aabd86fc711dfa006ca87
Krzych <krzych.pierzchala@gmail.com>
2017-11-03 06:41:10 +0100

    Added appending list items from function showWeather

--- script.js
+++ script.js
@@ -1,69 +1,95 @@
-var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Warsaw%22)%20and%20u%3D%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
-var citiesRandom = [];
+$(document).ready(function() {
+ 
+  var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Warsaw%22)%20and%20u%3D%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+  var citiesRandom = [];
 
-var cities = ['Lodz', 'Warsaw', 'Berlin', 'New York', 'London'];
+  var cities = ['Lodz', 'Warsaw', 'Berlin', 'New York', 'London'];
 
-var $btn = $('#get-weather');
-var $list = $('.list');
+  var $btn = $('#get-weather');
+  var $list = $('#city-list');
+  //var $item = $('<li>').attr('class', 'list-group-item');
 
-function randomCities (array) {
-	citiesRandom = [];
-	var randomIndex = 0;
-	var city = '';
-	while (citiesRandom.length < 3i) {
-		randomIndex = Math.floor(Math.random() * 4);
-		city = array[randomIndex];
-		console.log(city);
-		if (citiesRandom.indexOf(city) < 0) citiesRandom.push(array[randomIndex]);
-	}
-		
-	console.log(citiesRandom);
-	return citiesRandom;
-}
+  function randomCities(array) {
+    citiesRandom = [];
+    var randomIndex = 0;
+    var city = '';
+    while (citiesRandom.length < 3) {
+      randomIndex = Math.floor(Math.random() * 4);
+      city = array[randomIndex];
+      console.log(city);
+      if (citiesRandom.indexOf(city) < 0) citiesRandom.push(array[randomIndex]);
+    }
 
-fillData();
-//var intervalCities = setInterval(fillData, 5000);
+    console.log(citiesRandom);
+    return citiesRandom;
+  }
 
+  fillData();
+  var intervalCities = setInterval(function() {
+    clearInterval();
+    fillData(); 
+    //var intervalData = setInterval(intervalRequest, 5000);
+  }, 10000);
 
-//randomCities(cities);
+  //randomCities(cities);
 
-function getRequest(city) {
-	$.ajax({
-		url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + city + '%22)%20and%20u%3D%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
-		method: 'get',
-		success: showWeather
-	});	
-}
+  function getRequest(city) {
+    $.ajax({
+      url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + city + '%22)%20and%20u%3D%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
+      method: 'get',
+      success: showWeather
+    });
+  }
 
-function showWeather (resp) {
-	console.log(resp);
-	$('<li>').text(resp.query.results.channel.location.city + ': ' + resp.query.results.channel.item.condition.temp).append($('<sup>').text('o')).append('C, ' + resp.query.results.channel.item.condition.text).appendTo($list);
-}
+  function showWeather(resp) {
+    //console.log(resp);
+    //clearContainer();
+    var $item = $('<li>').attr('class', 'list-group-item');
+    var cityName = resp.query.results.channel.location.city;
+    var temp = resp.query.results.channel.item.condition.temp;
+    var text = resp.query.results.channel.item.condition.text;
+    var $celcius = $('<sup>').text(' o');
+    var $imgUrl = resp.query.results.channel.item.description;
+    var $imgString = $imgUrl.substr(9, 50);
+    var x = '<img src="http://l.yimg.com/a/i/us/we/52/27.gif"/>'
+    $img = $($imgString);
+    //$img.appendTo('main');
 
-function tripleRequest() {
-	for (var i = 0; i < citiesRandom.length; i++) {
-		getRequest(citiesRandom[i]);
-	}
-}
+    $item.text(cityName + ': ');
+    $item.append(temp);
+    $item.append($celcius);
+    $item.append('C, ' + text);
+    $item.append($img);
+    $item.appendTo($list);
+  }
 
-function fillData() {
-	clearContainer();
-	randomCities(cities);
-	tripleRequest();
-	//var intervalData = setInterval(tripleRequest, 3000);
-}
+  function tripleRequest(cities) {
+    console.log('Get');
+    clearContainer();
+    for (var i = 0; i < cities.length; i++) {
+      getRequest(cities[i]);
+    }
+  }
 
-$btn.click(function() {
-	clearContainer();
-	randomCities(cities);
-	for (var i = 0; i < citiesRandom.length; i++) {
-		getRequest(citiesRandom[i]);
-	}
-	console.log(cities);
+  function intervalRequest() {
+    console.log('Interval Get');
+    clearContainer();
+    for (var i = 0; i < citiesRandom.length; i++) {
+      getRequest(citiesRandom[i]);
+    }
+  }
+
+  //tripleRequest();
+
+  function fillData() {
+    clearContainer();
+    tripleRequest(randomCities(cities));
+    //var intervalData = setInterval(intervalRequest, 5000);
+  }
+
+  function clearContainer() {
+    $list.empty();
+    console.log('cleaner');
+  }
+
 });
-
-function clearContainer() {
-	$list.empty();
-	console.log('Cleaning container');
-}
-
